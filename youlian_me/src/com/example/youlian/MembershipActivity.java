4d371c328e8f4e6fdeca4975ae2a7562d7a8d285
wangweijun <wangweijun@PC2011030822ago>
2014-01-05 23:04:32 +0800

    会员卡

--- youlian_me/src/com/example/youlian/MembershipActivity.java
+++ youlian_me/src/com/example/youlian/MembershipActivity.java
@@ -3,15 +3,43 @@ package com.example.youlian;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class MembershipActivity extends Activity {
-  // ª·‘±ø®
+public class MembershipActivity extends Activity implements OnClickListener{
 
-    @Override
+    private ImageButton back;
+	private TextView tv_title;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_membership);
+        
+        initViews();
+        
+        
     }
 
+	private void initViews() { 
+		back = (ImageButton)this.findViewById(R.id.back);
+		back.setOnClickListener(this);
+		tv_title = (TextView)this.findViewById(R.id.tv_title);
+		tv_title.setText(R.string.membership);
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.back:
+			finish();
+			break;
+
+		default:
+			break;
+		}
+	}
     
 }
