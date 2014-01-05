a692f4fdde490a2e5887e8ace7b77c0e5d20ad08
wangweijun <wangweijun@PC2011030822ago>
2014-01-05 23:17:40 +0800

    会员卡adapter

--- youlian_me/src/com/example/youlian/MembershipActivity.java
+++ youlian_me/src/com/example/youlian/MembershipActivity.java
@@ -1,17 +1,24 @@
 package com.example.youlian;
 
+import com.android.volley.toolbox.NetworkImageView;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.BaseAdapter;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MembershipActivity extends Activity implements OnClickListener{
 
     private ImageButton back;
 	private TextView tv_title;
+	private ListView listview;
+	private MyAdapter adapter;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,10 @@ public class MembershipActivity extends Activity implements OnClickListener{
 		back.setOnClickListener(this);
 		tv_title = (TextView)this.findViewById(R.id.tv_title);
 		tv_title.setText(R.string.membership);
+		
+		listview = (ListView) this.findViewById(R.id.listview);
+		adapter = new MyAdapter(getApplicationContext());
+		listview.setAdapter(adapter);
 	}
 
 	@Override
@@ -40,6 +51,73 @@ public class MembershipActivity extends Activity implements OnClickListener{
 		default:
 			break;
 		}
+	}
+	
+	
+	private class MyAdapter extends BaseAdapter {
+
+		public MyAdapter(Context context) {
+		}
+
+
+		@Override
+		public int getCount() {
+	        return 0;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+//			ViewHolder holder = null;
+//			if (convertView == null) {
+//				convertView = inflater.inflate(R.layout.item_my_favorite,
+//						parent, false);
+//				holder = new ViewHolder();
+//				holder.iv_channel_icon = (NetworkImageView) convertView.findViewById(R.id.iv_channel_icon);
+//				holder.tv_channel_title = (TextView) convertView.findViewById(R.id.tv_channel_title);
+//				holder.tv_jiemu_name = (TextView) convertView.findViewById(R.id.tv_jiemu_name);
+//				holder.icon = (NetworkImageView) convertView.findViewById(R.id.iv_icon);
+//				holder.favorite = (LinearLayout) convertView.findViewById(R.id.linear_cancel);
+//				holder.favorite.setOnClickListener(FavoriteActivity.this);
+//				holder.play = (LinearLayout) convertView.findViewById(R.id.linear_play);
+//				holder.play.setOnClickListener(FavoriteActivity.this);
+//				convertView.setTag(holder);
+//			} else {
+//				holder = (ViewHolder) convertView.getTag();
+//			}
+//
+//			setValue(holder, position);
+
+			return convertView;
+		}
+
+		public void setValue(ViewHolder holder, int position) {
+			
+		}
+
+		class ViewHolder {
+			public NetworkImageView iv_channel_icon;
+			
+			public TextView tv_channel_title;
+			public TextView tv_jiemu_name;
+			public NetworkImageView icon;
+			public LinearLayout favorite;
+			public LinearLayout play;
+		}
+
+	
+
+
 	}
     
 }
