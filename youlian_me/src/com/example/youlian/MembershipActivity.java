20970d597a9360741083446dde81d98610ea73e6
wangweijun <wangweijun@PC2011030822ago>
2014-01-07 00:33:35 +0800

    会员卡列表

--- youlian_me/src/com/example/youlian/MembershipActivity.java
+++ youlian_me/src/com/example/youlian/MembershipActivity.java
@@ -1,33 +1,24 @@
 package com.example.youlian;
 
-import java.util.List;
-
-import org.json.JSONException;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.BaseAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.NetworkImageView;
-import com.example.youlian.mode.SubjectActivity;
-import com.example.youlian.view.TemplateFive;
-import com.example.youlian.view.TemplateFour;
-import com.example.youlian.view.TemplateOne;
-import com.example.youlian.view.TemplateSix;
-import com.example.youlian.view.TemplateThree;
-import com.example.youlian.view.TemplateTwo;
-
-import android.os.Bundle;
-import android.app.Activity;
-import android.content.Context;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.View.OnClickListener;
-import android.widget.BaseAdapter;
-import android.widget.ImageButton;
-import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.TextView;
 
 public class MembershipActivity extends Activity implements OnClickListener{
 
@@ -36,6 +27,7 @@ public class MembershipActivity extends Activity implements OnClickListener{
 	private TextView tv_title;
 	private ListView listview;
 	private MyAdapter adapter;
+	private LayoutInflater inflater;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +45,7 @@ public class MembershipActivity extends Activity implements OnClickListener{
 		back.setOnClickListener(this);
 		tv_title = (TextView)this.findViewById(R.id.tv_title);
 		tv_title.setText(R.string.membership);
-		
+		inflater = LayoutInflater.from(getApplicationContext());
 		listview = (ListView) this.findViewById(R.id.listview);
 		adapter = new MyAdapter(getApplicationContext());
 		listview.setAdapter(adapter);
@@ -80,7 +72,7 @@ public class MembershipActivity extends Activity implements OnClickListener{
 
 		@Override
 		public int getCount() {
-	        return 0;
+	        return 10;
 		}
 
 		@Override
@@ -96,25 +88,27 @@ public class MembershipActivity extends Activity implements OnClickListener{
 
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-//			ViewHolder holder = null;
-//			if (convertView == null) {
-//				convertView = inflater.inflate(R.layout.item_my_favorite,
-//						parent, false);
-//				holder = new ViewHolder();
-//				holder.iv_channel_icon = (NetworkImageView) convertView.findViewById(R.id.iv_channel_icon);
-//				holder.tv_channel_title = (TextView) convertView.findViewById(R.id.tv_channel_title);
-//				holder.tv_jiemu_name = (TextView) convertView.findViewById(R.id.tv_jiemu_name);
-//				holder.icon = (NetworkImageView) convertView.findViewById(R.id.iv_icon);
-//				holder.favorite = (LinearLayout) convertView.findViewById(R.id.linear_cancel);
-//				holder.favorite.setOnClickListener(FavoriteActivity.this);
-//				holder.play = (LinearLayout) convertView.findViewById(R.id.linear_play);
-//				holder.play.setOnClickListener(FavoriteActivity.this);
-//				convertView.setTag(holder);
-//			} else {
-//				holder = (ViewHolder) convertView.getTag();
-//			}
-//
-//			setValue(holder, position);
+			ViewHolder holder = null;
+			if (convertView == null) {
+				convertView = inflater.inflate(R.layout.item_membership,
+						parent, false);
+				holder = new ViewHolder();
+				holder.iv_icon = (ImageView) convertView.findViewById(R.id.iv_icon);
+				holder.tv_title = (TextView) convertView.findViewById(R.id.tv_title);
+				holder.iv_star_one = (ImageView) convertView.findViewById(R.id.iv_star_one);
+				holder.iv_star_two = (ImageView) convertView.findViewById(R.id.iv_star_two);
+				holder.iv_star_three = (ImageView) convertView.findViewById(R.id.iv_star_three);
+				holder.iv_star_four = (ImageView) convertView.findViewById(R.id.iv_star_four);
+				holder.iv_star_five = (ImageView) convertView.findViewById(R.id.iv_star_five);
+				holder.iv_online_chong = (ImageView) convertView.findViewById(R.id.iv_online_chong);
+				holder.tv_cardname = (TextView) convertView.findViewById(R.id.tv_cardname);
+				holder.tv_desc = (TextView) convertView.findViewById(R.id.tv_desc);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+
+			setValue(holder, position);
 
 			return convertView;
 		}
@@ -124,13 +118,11 @@ public class MembershipActivity extends Activity implements OnClickListener{
 		}
 
 		class ViewHolder {
-			public NetworkImageView iv_channel_icon;
-			
-			public TextView tv_channel_title;
-			public TextView tv_jiemu_name;
-			public NetworkImageView icon;
-			public LinearLayout favorite;
-			public LinearLayout play;
+			public ImageView iv_icon;
+			public TextView tv_title;
+			public ImageView iv_star_one, iv_star_two, iv_star_three, iv_star_four, iv_star_five;
+			public ImageView iv_online_chong;
+			TextView tv_cardname, tv_desc;
 		}
 
 	
