0577d304ea3437036e2683318532d5c431afd8d9
wangweijun <wangweijun@PC2011030822ago>
2014-01-08 00:21:14 +0800

    会员卡 全部分类

--- youlian_me/src/com/example/youlian/MembershipActivity.java
+++ youlian_me/src/com/example/youlian/MembershipActivity.java
@@ -20,36 +20,63 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.NetworkImageView;
 
-public class MembershipActivity extends Activity implements OnClickListener{
+public class MembershipActivity extends Activity implements OnClickListener {
 
-    protected static final String TAG = "MembershipActivity";
+	protected static final String TAG = "MembershipActivity";
 	private ImageButton back;
 	private TextView tv_title;
 	private ListView listview;
 	private MyAdapter adapter;
 	private LayoutInflater inflater;
+	private LinearLayout linear_all_area;
+	private LinearLayout linear_all_sort;
+	private LinearLayout linear_all_hot;
+	private ListView listview_all;
+	private MyAdapterAll adapterAll;
+	private static final int allarea = 1;
+	private static final int allsort = 2;
+	private static final int hot = 3;
+	private int type = allarea;
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_membership);
-        
-        initViews();
-        
-        YouLianHttpApi.getMemberCard(null, null, createMyReqSuccessListener(), createMyReqErrorListener());
-    }
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_membership);
 
-	private void initViews() { 
-		back = (ImageButton)this.findViewById(R.id.back);
+		initViews();
+
+		YouLianHttpApi.getMemberCard(null, null, createMyReqSuccessListener(),
+				createMyReqErrorListener());
+	}
+
+	private void initViews() {
+		back = (ImageButton) this.findViewById(R.id.back);
 		back.setOnClickListener(this);
-		tv_title = (TextView)this.findViewById(R.id.tv_title);
+		tv_title = (TextView) this.findViewById(R.id.tv_title);
 		tv_title.setText(R.string.membership);
 		inflater = LayoutInflater.from(getApplicationContext());
 		listview = (ListView) this.findViewById(R.id.listview);
 		adapter = new MyAdapter(getApplicationContext());
 		listview.setAdapter(adapter);
+		listview.setVisibility(View.GONE);
+
+		linear_all_area = (LinearLayout) this
+				.findViewById(R.id.linear_all_area);
+		linear_all_sort = (LinearLayout) this
+				.findViewById(R.id.linear_all_sort);
+		linear_all_hot = (LinearLayout) this.findViewById(R.id.linear_all_hot);
+		linear_all_area.setOnClickListener(this);
+		linear_all_sort.setOnClickListener(this);
+		linear_all_hot.setOnClickListener(this);
+
+		listview_all = (ListView) this.findViewById(R.id.listview_all);
+		adapterAll = new MyAdapterAll(getApplicationContext());
+		listview_all.setAdapter(adapterAll);
+//		listview_all.setVisibility(View.GONE);
 	}
+
+	boolean exChange = true;
 
 	@Override
 	public void onClick(View v) {
@@ -57,22 +84,44 @@ public class MembershipActivity extends Activity implements OnClickListener{
 		case R.id.back:
 			finish();
 			break;
+		case R.id.linear_all_area:
+			type = allarea;
+//			exChange();
+			adapterAll.notifyDataSetChanged();
+			break;
+		case R.id.linear_all_sort:
+			type = allsort;
+			adapterAll.notifyDataSetChanged();
+			break;
+		case R.id.linear_all_hot:
+			type = hot;
+			adapterAll.notifyDataSetChanged();
+			break;
 
 		default:
 			break;
 		}
 	}
-	
-	
+
+	private void exChange() {
+		if (exChange) {
+			listview.setVisibility(View.GONE);
+			listview_all.setVisibility(View.VISIBLE);
+		} else {
+			listview.setVisibility(View.VISIBLE);
+			listview_all.setVisibility(View.GONE);
+		}
+		exChange = !exChange;
+	}
+
 	private class MyAdapter extends BaseAdapter {
 
 		public MyAdapter(Context context) {
 		}
 
-
 		@Override
 		public int getCount() {
-	        return 10;
+			return 10;
 		}
 
 		@Override
@@ -93,16 +142,26 @@ public class MembershipActivity extends Activity implements OnClickListener{
 				convertView = inflater.inflate(R.layout.item_membership,
 						parent, false);
 				holder = new ViewHolder();
-				holder.iv_icon = (ImageView) convertView.findViewById(R.id.iv_icon);
-				holder.tv_title = (TextView) convertView.findViewById(R.id.tv_title);
-				holder.iv_star_one = (ImageView) convertView.findViewById(R.id.iv_star_one);
-				holder.iv_star_two = (ImageView) convertView.findViewById(R.id.iv_star_two);
-				holder.iv_star_three = (ImageView) convertView.findViewById(R.id.iv_star_three);
-				holder.iv_star_four = (ImageView) convertView.findViewById(R.id.iv_star_four);
-				holder.iv_star_five = (ImageView) convertView.findViewById(R.id.iv_star_five);
-				holder.iv_online_chong = (ImageView) convertView.findViewById(R.id.iv_online_chong);
-				holder.tv_cardname = (TextView) convertView.findViewById(R.id.tv_cardname);
-				holder.tv_desc = (TextView) convertView.findViewById(R.id.tv_desc);
+				holder.iv_icon = (ImageView) convertView
+						.findViewById(R.id.iv_icon);
+				holder.tv_title = (TextView) convertView
+						.findViewById(R.id.tv_title);
+				holder.iv_star_one = (ImageView) convertView
+						.findViewById(R.id.iv_star_one);
+				holder.iv_star_two = (ImageView) convertView
+						.findViewById(R.id.iv_star_two);
+				holder.iv_star_three = (ImageView) convertView
+						.findViewById(R.id.iv_star_three);
+				holder.iv_star_four = (ImageView) convertView
+						.findViewById(R.id.iv_star_four);
+				holder.iv_star_five = (ImageView) convertView
+						.findViewById(R.id.iv_star_five);
+				holder.iv_online_chong = (ImageView) convertView
+						.findViewById(R.id.iv_online_chong);
+				holder.tv_cardname = (TextView) convertView
+						.findViewById(R.id.tv_cardname);
+				holder.tv_desc = (TextView) convertView
+						.findViewById(R.id.tv_desc);
 				convertView.setTag(holder);
 			} else {
 				holder = (ViewHolder) convertView.getTag();
@@ -114,41 +173,87 @@ public class MembershipActivity extends Activity implements OnClickListener{
 		}
 
 		public void setValue(ViewHolder holder, int position) {
-			
 		}
 
 		class ViewHolder {
 			public ImageView iv_icon;
 			public TextView tv_title;
-			public ImageView iv_star_one, iv_star_two, iv_star_three, iv_star_four, iv_star_five;
+			public ImageView iv_star_one, iv_star_two, iv_star_three,
+					iv_star_four, iv_star_five;
 			public ImageView iv_online_chong;
 			TextView tv_cardname, tv_desc;
 		}
-
-	
-
-
 	}
-	
-	
+
+	private class MyAdapterAll extends BaseAdapter {
+
+		public MyAdapterAll(Context context) {
+		}
+
+		@Override
+		public int getCount() {
+			return 1;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			convertView = inflater.inflate(R.layout.item_all, parent, false);
+			ImageView iv = (ImageView) convertView.findViewById(R.id.iv_icon);
+			TextView tv = (TextView) convertView.findViewById(R.id.tv_title);
+			switch (type) {
+			case allarea:
+				
+				break;
+			case allsort:
+				iv.setVisibility(View.GONE);
+				break;
+			case hot:
+				tv.setVisibility(View.GONE);
+				break;
+
+			}
+
+			return convertView;
+		}
+
+		class ViewHolder {
+			public ImageView iv_icon;
+			public TextView tv_title;
+			public ImageView iv_star_one, iv_star_two, iv_star_three,
+					iv_star_four, iv_star_five;
+			public ImageView iv_online_chong;
+			TextView tv_cardname, tv_desc;
+		}
+	}
+
 	private Response.Listener<String> createMyReqSuccessListener() {
-        return new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-            	Log.i(TAG, "success:" + response);
-            	
-            }
-        };
-    }
+		return new Response.Listener<String>() {
+			@Override
+			public void onResponse(String response) {
+				Log.i(TAG, "success:" + response);
 
+			}
+		};
+	}
 
-    private Response.ErrorListener createMyReqErrorListener() {
-        return new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            	Log.i(TAG, "error");
-            }
-        };
-    }
-    
+	private Response.ErrorListener createMyReqErrorListener() {
+		return new Response.ErrorListener() {
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				Log.i(TAG, "error");
+			}
+		};
+	}
+
 }
